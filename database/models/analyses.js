cabf1a831e406c8fd3f3109bbc54d714e3ecbcb7
Eddie Chou <eddieechou@gmail.com>
2017-03-28 15:09:10 -0700

    (refactor) Code cleanup

--- database/models/analyses.js
+++ database/models/analyses.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 
 var AnalysesSchema = mongoose.Schema({
-	person: String,
-	context: String,
+  person: String,
+  context: String,
   word_count: Number,
   private: Boolean,
-	user_id: String
-})
+  user_id: String
+});
 
 var Analysis = mongoose.model('Analysis', AnalysesSchema);
 
--- database/models/users.js
+++ database/models/users.js
@@ -2,31 +2,31 @@ var mongoose = require('mongoose');
 var crypto = require('crypto');
 
 var UsersSchema = mongoose.Schema({
-	username: String,
-	email: String,
-	password: String,
-	salt: String
-	//date is created automatically as apart of mongodb, accessed by the _id object, should we implement our own date for this?
+  username: String,
+  email: String,
+  password: String,
+  salt: String
+  //date is created automatically as apart of mongodb, accessed by the _id object, should we implement our own date for this?
 });
 
 UsersSchema.pre('save', function(next) {
-	crypto.randomBytes(40, function(err, salt) {
-		if (err) {
-			console.log('there as an error making a user\'s salt', err)
-		} else {
-			var hash = crypto.createHash('sha256');
-			this.salt = salt.toString();
-			hash.update(this.password + this.salt);
-			this.password = hash.digest('hex');
-			next();
-		}
- 	}.bind(this));
-})
+  crypto.randomBytes(40, function(err, salt) {
+    if (err) {
+      console.log('there as an error making a user\'s salt', err);
+    } else {
+      var hash = crypto.createHash('sha256');
+      this.salt = salt.toString();
+      hash.update(this.password + this.salt);
+      this.password = hash.digest('hex');
+      next();
+    }
+  }.bind(this));
+});
 
 var User = mongoose.model('User', UsersSchema);
 
 User.populateTestData = function() {
-	User.findOne({username: 'TestUser'}, function(err, found) {
+  User.findOne({username: 'TestUser'}, function(err, found) {
     if (err) { console.error(err); }
     if (!found) {
       var testUser = new User({
@@ -41,10 +41,10 @@ User.populateTestData = function() {
 };
 
 User.comparePassword = function(attemptedPassword, salt, actualPassword) {
-	var hash = crypto.createHash('sha256');
-	hash.update(attemptedPassword + salt);
-	attemptedPasswordHash = hash.digest('hex');
-	return (actualPassword === attemptedPasswordHash);	
+  var hash = crypto.createHash('sha256');
+  hash.update(attemptedPassword + salt);
+  attemptedPasswordHash = hash.digest('hex');
+  return (actualPassword === attemptedPasswordHash);  
 };
 
 module.exports = User;

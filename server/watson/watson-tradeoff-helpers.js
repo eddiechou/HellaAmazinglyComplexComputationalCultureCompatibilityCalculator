4628680f50aab1a42ea0923af0b4ae3dcb606fb2
Eddie Chou <eddieechou@gmail.com>
2017-03-29 10:24:37 -0700

    (feat) Implement createColumns method that returns default data for now.
    Set up promise chain to create the PromiseObject.

--- server/watson/watson-tradeoff-helpers.js
+++ server/watson/watson-tradeoff-helpers.js
@@ -25,46 +25,99 @@ var AnalysesTrait = require('../../database/models/analyses_traits');
 //   }
 // ]
 var createOptions = function(callback) {
-  var options = [];
+  return new Promise((resolve, reject) => {
 
-  var promises;
-  // Query for the analyses we want (all public profiles FOR NOW)
-  Analysis.find({private: false})
-  .exec(function(err, analysesArray) {
-    if (err) { 
-      res.status(500).send(JSON.stringify({error: 'Databases failed to query'})); 
-    } else if (analysesArray) {
+    var options = [];
 
-      promises = analysesArray.map(function(analysis) {
-        // return a promise for each analysis
-        return new Promise((resolve, reject) => {
-          // Set up key and name
-          var obj = {
-            key: analysis._id,
-            name: analysis.person
-          };
+    var promises;
+    // Query for the analyses we want (all public profiles FOR NOW)
+    Analysis.find({private: false})
+    .exec(function(err, analysesArray) {
+      if (err) { 
+        res.status(500).send(JSON.stringify({error: 'Databases failed to query'})); 
+      } else if (analysesArray) {
 
-          // Set up values object
-          var values = {};
-          // Query analyses_traits for all traits with this analysis_id
-          AnalysesTrait.find({analysis_id: analysis._id})
-          .exec(function(err, analysisTraits) {
-            if (err) {
-              console.log('AnalysesTrait.find() err', err);
-              reject(err);
-            } else if (analysisTraits) {
-              for (var j = 0; j < analysisTraits.length; j++) {
-                values[analysisTraits[j].trait_id] = analysisTraits[j].percentile.valueOf();
+        promises = analysesArray.map(function(analysis) {
+          // return a promise for each analysis
+          return new Promise((resolve, reject) => {
+            // Set up key and name
+            var obj = {
+              key: analysis._id,
+              name: analysis.person
+            };
+
+            // Set up values object
+            var values = {};
+            // Query analyses_traits for all traits with this analysis_id
+            AnalysesTrait.find({analysis_id: analysis._id})
+            .exec(function(err, analysisTraits) {
+              if (err) {
+                console.log('AnalysesTrait.find() err', err);
+                reject(err);
+              } else if (analysisTraits) {
+                for (var j = 0; j < analysisTraits.length; j++) {
+                  values[analysisTraits[j].trait_id] = analysisTraits[j].percentile.valueOf();
+                }
               }
-            }
-            obj.values = values;
-            options.push(obj);
-            resolve();
+              obj.values = values;
+              options.push(obj);
+              // resolve(options);
+              resolve();
+            });
           });
         });
-      });
-      Promise.all(promises).then(() => callback(options));
-    }
+        // Promise.all(promises).then(() => callback(options));
+        Promise.all(promises).then(() => resolve(options));
+      }
+    });
+  });
+};
+
+var createColumns = function(callback) {
+  return new Promise((resolve, reject) => {
+
+    // Test columns
+    var columns = [
+      {
+        "key": "facet_adventurousness",
+        "type": "numeric",
+        "goal": "max",
+        "is_objective": true,
+        "full_name": "Adventuresness",
+        "range": {
+          "low": 0,
+          "high": 100
+        },
+        "format": "number:0"
+      },
+      {
+        "key": "facet_emotionality",
+        "type": "numeric",
+        "goal": "min",
+        "is_objective": true,
+        "full_name": "Emotionality",
+        "format": "number:0",
+        "range": {
+          "low": 0,
+          "high": 100
+        }
+      },
+      {
+        "key": "facet_intellect",
+        "type": "numeric",
+        "goal": "max",
+        "is_objective": true,
+        "full_name": "Intellect",
+        "format": "number:0",
+        "range": {
+          "low": 0,
+          "high": 100
+        }
+      },
+    ];
+
+    resolve(columns);
+
   });
 };
 
@@ -83,8 +136,12 @@ var analyzeTradeoffs = function(req, res) {
     options: null
   };
   // Populate with columns and options
-  createOptions(function(options) {
-    console.log('return value from createOptions: ', options);
+  createColumns()
+  .then(function(columns) {
+    problemObj.columns = columns;
+    return createOptions();
+  })
+  .then(function(options) {
     problemObj.options = options;
     res.send(JSON.stringify(problemObj));
   });
@@ -92,3 +149,4 @@ var analyzeTradeoffs = function(req, res) {
 
 module.exports.analyzeTradeoffs = analyzeTradeoffs;
 module.exports.createOptions = createOptions;
+module.exports.createColumns = createColumns;

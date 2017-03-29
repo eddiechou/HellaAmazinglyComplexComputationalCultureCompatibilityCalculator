eae6d5eb1806424fe65b36ae5b0b01bbff6a2df9
Eddie Chou <eddieechou@gmail.com>
2017-03-28 19:44:35 -0700

    (refactor) Add example options array and add Todo for Problem object creation

--- server/watson/watson-tradeoff-helpers.js
+++ server/watson/watson-tradeoff-helpers.js
@@ -1,6 +1,29 @@
 var Analysis = require('../../database/models/analyses');
 var AnalysesTrait = require('../../database/models/analyses_traits');
 
+// Example options array
+// [
+//   {
+//     "key": "1",
+//     "name": "Samsung Galaxy S4",
+//     "values": {
+//       "price": 249,
+//       "weight": 130,
+//       "brand": "Samsung",
+//       "rDate": "2013-04-29T00:00:00Z"
+//     }
+//   },
+//   {
+//     "key": "2",
+//     "name": "Apple iPhone 5",
+//     "values": {
+//       "price": 349,
+//       "weight": 112,
+//       "brand": "Apple",
+//       "rDate": "2012-09-21T00:00:00Z"
+//     }
+//   }
+// ]
 var createOptions = function(callback) {
   var options = [];
 
@@ -57,8 +80,16 @@ var createOptions = function(callback) {
   });
 };
 
+// TODO (Eddie): Right now, it only sends back the options array as a response,
+// still need to:
+// 1) create the whole Problem object 
+// 2) send whole Problem object to Watson Tradeoff API
+// 3) deal with result
+
 // Sends a POST request with a Problem object to the Watson Tradeoff API
+// Problem object contains: subject, columns, and options
 var analyzeTradeoffs = function(req, res) {
+
   createOptions(function(options) {
     console.log(options);
     res.send(JSON.stringify(options));

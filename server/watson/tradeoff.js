7d063cba29877fbc598bb9dd30cadfb027e17c86
Eddie Chou <eddieechou@gmail.com>
2017-03-29 15:17:09 -0700

    (feat) Implement API call to the Watson Tradeoff Analytics API

---
+++ server/watson/tradeoff.js
@@ -0,0 +1,20 @@
+var TradeoffAnalyticsV1 = require('watson-developer-cloud/tradeoff-analytics/v1');
+var tradeoffAnalytics = new TradeoffAnalyticsV1({
+  username: process.env.T_A_USERNAME,
+  password: process.env.T_A_PASSWORD
+});
+
+// Parameters for the call are defined in the problem.json file.
+// var params = require('problem.json');
+
+var getDilemma = function(params) {
+  tradeoffAnalytics.dilemmas(params, function(error, resolution) {
+    if (error) {
+      console.log('Error in getDilemma:', error);
+    } else {
+      console.log('getDilemma resolution: ', JSON.stringify(resolution, null, 2));
+    }
+  });
+};
+
+module.exports.getDilemma = getDilemma;

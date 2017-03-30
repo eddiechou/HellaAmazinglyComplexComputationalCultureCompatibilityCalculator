d5b041220ba82c535a291fee29d0621b52eaee9b
Jason Chambers <jas.o.chambers@gmail.com>
2017-03-30 11:53:50 -0700

    (feat) lay out structure in social/auth0 file for saving users in db on successful login

--- server/social/auth0.js
+++ server/social/auth0.js
@@ -1,5 +1,12 @@
-var passport = require('passport');
-var Auth0Strategy = require('passport-auth0');
+import passport from 'passport';
+import Auth0Strategy from 'passport-auth0';
+import dbHelpers from '../../database/helpers/request_helpers';
+
+const providers = {
+  auth0: dbHelpers.auth0UserStore,
+  github: dbHelpers.githubUserStore,
+  facebook: dbHelpers.facebookUserStore
+}
 
 // Configure Passport to use Auth0
 var strategy = new Auth0Strategy({
@@ -8,7 +15,14 @@ var strategy = new Auth0Strategy({
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:  '/callback'
   }, function(accessToken, refreshToken, extraParams, profile, done) {
-    return done(null, profile);
+    
+    console.log(providers[profile.provider]);
+
+    // route to specific helper func based on login provider
+    providers[profile.provider](profile, (param) => {
+      return done(null, profile);
+    });
+
   });
 
 passport.use(strategy);

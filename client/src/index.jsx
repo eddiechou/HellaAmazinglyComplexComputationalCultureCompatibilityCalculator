1dc4e3a107f2a650d3b5f3de3abc75d6a9af8a69
Jason Chambers <jas.o.chambers@gmail.com>
2017-03-31 15:15:05 -0700

    (feat) added twitter events widget on page render

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -31,6 +31,25 @@ class App extends React.Component {
 
   }
 
+  componentWillMount() {
+    window.twttr = (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+      if (d.getElementById(id)) return t;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://platform.twitter.com/widgets.js";
+      fjs.parentNode.insertBefore(js, fjs);
+
+      t._e = [];
+      t.ready = function(f) {
+        t._e.push(f);
+      };
+
+      return t;
+  }(document, "script", "twitter-wjs"));
+  }
+
   componentDidMount() {
     s.serverGet('checkLoggedIn').then((res) => {
       console.log(res);
@@ -112,6 +131,7 @@ class App extends React.Component {
 
             </div>
           </nav>
+          
           <div className="container">
             {this.state.spinner && <img id="spinner" className="header" src={"/images/spinner.gif"} />}
             <Route path="/Home" component={About}/>
